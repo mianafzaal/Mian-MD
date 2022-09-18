af1d10b45f0246fa7304dc58a8d81f0037b3d05d
mian <96155069+mianafzaal@users.noreply.github.com>
2022-09-18 05:17:44 +0500

    Add files via upload

--- config.js
+++ config.js
@@ -7,15 +7,15 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
-global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
-global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
+global.Owner = ['923430436852']
+global.OwnerNumber = ['923430436852']
+global.ownertag = ['923430436852']
+global.BotName = "Mian"
+global.packname = "Mian bot"
+global.author = "By: Mian"
+global.OwnerName = "Mina"
+global.BotSourceCode = "😃😃"
+global.SupportGroupLink = "😃🤓"
 global.sessionName = "session"
 
 // Prefix //
@@ -23,11 +23,11 @@ global.sessionName = "session"
 global.prefa = ['-'] 
 
 
-global.location = "West Bengal, India"
+global.location = "Pakistan"
 global.reactmoji = "❤️"
 global.themeemoji = "💖"
 global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
-global.websitex = "https://github.com/FantoX001"
+global.websitex = "😃🤓"
 global.lolhuman = "KaysaS"
 
 
